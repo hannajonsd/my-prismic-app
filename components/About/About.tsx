372d1d83bcc051ea79361acdb4d7ab6f7d236164
Hanna Margrét Jónsdóttir <hanna.m.jonsd@gmail.com>
2024-04-11 14:52:17 +0000

    prettier

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -5,7 +5,7 @@ export function About({ text, picture }: { text: string, picture?: any}) {
     return (
         <div className={s.header}>
             <h1 className={s.title}>{text}</h1> 
-            {picture && <img src={picture} alt={'image of album'} className={s.img} />}
+            {picture && <img src={picture} alt={'image'} className={s.img} />}
         </div>
     );
 }
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import s from './home-styles.module.scss';
-import Link from 'next/link';
+
 
 export function Home({ text, picture }: { text: string, picture?: any}) {
     return (
         <div className={s.header}>
-            {picture && <img src={picture} alt={'image of album'} className={s.img} />}
+            {picture && <img src={picture} alt={'image'} className={s.img} />}
             <p className={s.text}>{text}</p>
         </div>
     );
